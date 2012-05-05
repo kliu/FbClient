b3ce8ebf86e225b503a6de2b9f058d6331210628
John Wright <mrjjwright@gmail.com>
2012-05-04 20:10:46 -0600

    Initial Commit

---
+++ FacebookClient/FacebookClient.h
@@ -0,0 +1,13 @@
+//
+//  FacebookClient.h
+//  FacebookClient
+//
+//  Created by John Wright on 5/4/12.
+//  Copyright (c) 2012 AirHeart. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface FacebookClient : NSObject
+
+@end
---
+++ FacebookClient/FacebookClient.m
@@ -0,0 +1,13 @@
+//
+//  FacebookClient.m
+//  FacebookClient
+//
+//  Created by John Wright on 5/4/12.
+//  Copyright (c) 2012 AirHeart. All rights reserved.
+//
+
+#import "FacebookClient.h"
+
+@implementation FacebookClient
+
+@end
